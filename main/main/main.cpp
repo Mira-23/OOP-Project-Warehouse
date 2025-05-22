064d008cad6beefcea2dfc973d7aab7852ff2261
Mira-23 <stanimira23@abv.bg>
2025-05-22 19:13:23 +0300

    Initial Commit

---
+++ main/main/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+	//Menu prototype. This will change into a class later on.
+	std::string command;
+	std::cin >> command;
+	while (command != "Exit")
+	{
+
+	}
+	std::cout << "Exiting the program..." << std::endl;
+}
