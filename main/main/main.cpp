ecaa429394a867b8a57f7e827e860132b4b91252
Mira-23 <80602490+Mira-23@users.noreply.github.com>
2025-06-03 20:40:19 +0300

    change to main

--- main/main/main.cpp
+++ main/main/main.cpp
@@ -7,6 +7,36 @@
 /// <returns></returns>
 int main()
 {
-	Menu<std::ostream> menu;
-	menu.start(std::cout);
+	//Menu<std::ostream> menu;
+	//menu.start(std::cout);
+
+	//examples:
+	std::stringstream ss;
+	Menu<std::stringstream> string_menu;
+
+	string_menu.open("open test.txt",ss);
+
+	string_menu.add("add Milk|2025-06-22|2025-06-28|Pilos|2|Litres|Just milk",ss);
+	string_menu.add("add Milk|2025-06-22|2025-06-30|Pilos|3|Litres|Just milk",ss);
+	string_menu.add("add Apple|2025-06-22|2025-06-30|Big Farm|3|Kilograms|Just apples",ss);
+
+	string_menu.print(ss);
+
+	string_menu.check_losses("checklosses Milk|3|2|2025-05-29|2025-07-31",ss);
+
+	string_menu.add("add Apple|2023-06-22|2024-06-30|Big Farm|3|Kilograms|Just apples",ss);
+	string_menu.clean(ss);
+
+	string_menu.remove("remove Milk|1",ss);
+	string_menu.remove("remove Milk|1",ss);
+
+	string_menu.log("log 2024-05-29|2026-05-31",ss);
+
+	string_menu.print(ss);
+
+	string_menu.save(ss);
+
+	string_menu.close(ss);
+
+	std::cout << ss.str();
 }
